a964e557dafc28e0a2323fd6c095ba802b02bce0
nileshorpe46 <nilesh.orpe46@gmail.com>
2025-05-27 12:09:08 +0530

    commit from personal pc on 27-05

--- tests/uibasics.spec.js
+++ tests/uibasics.spec.js
@@ -9,5 +9,21 @@ test('page fixture test', async ({page})=>{
     await page.locator('input#password').fill('learning');
     await page.locator('input#signInBtn').click();
     // await expect(page.locator("div[style*='block']")).toContainText('Incorrect');
-    
+    console.log(await page.locator('h4.card-title a').nth(0).textContent());
+    console.log(await page.locator('h4.card-title a').first().textContent());
+    console.log(await page.locator('h4.card-title a').last().textContent());
 });
+
+
+test('wait mechanism test', async ({page})=>{
+    await page.goto('https://rahulshettyacademy.com/client');
+    await page.locator('input#userEmail').fill('anshika@gmail.com');
+    await page.locator('input#userPassword').fill('Iamking@000');
+    await page.locator('#login').click();
+    // await page.waitForLoadState('networkidle');
+    await page.locator('div.card-body b').last().waitFor();
+    let list = await page.locator('div.card-body b').allTextContents();
+    console.log(list);
+    
+
+})
